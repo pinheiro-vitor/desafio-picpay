7c5651dbd6b90b2fff17922c75b62dfb63d1576a
Vitor Pinheiro <vitor.pinheiro97@hotmail.com>
2025-02-27 20:27:55 -0300

    feat: create UserRepository

---
+++ src/main/java/com/desafiopicpay/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.desafiopicpay.repositories;
+
+import com.desafiopicpay.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
+}
