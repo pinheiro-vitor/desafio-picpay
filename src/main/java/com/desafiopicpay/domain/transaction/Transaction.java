f1eaee2811ea5f5553ca34e93be9ffa370b7e553
Vitor Pinheiro <vitor.pinheiro97@hotmail.com>
2025-02-27 20:27:35 -0300

    feat: create transaction entity

---
+++ src/main/java/com/desafiopicpay/domain/transaction/Transaction.java
@@ -0,0 +1,29 @@
+package com.desafiopicpay.domain.transaction;
+
+import com.desafiopicpay.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+}
