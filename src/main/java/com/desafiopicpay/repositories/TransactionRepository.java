4172818669d9d198067f9c261a76c28d19ee2265
Vitor Pinheiro <vitor.pinheiro97@hotmail.com>
2025-02-27 20:30:51 -0300

    fix: interface TransactionRepository

--- src/main/java/com/desafiopicpay/repositories/TransactionRepository.java
+++ src/main/java/com/desafiopicpay/repositories/TransactionRepository.java
@@ -1,5 +1,6 @@
 package com.desafiopicpay.repositories;
 
+import com.desafiopicpay.domain.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
